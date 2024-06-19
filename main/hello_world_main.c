cfc92be85efec048cd2cc52802f1846a5c0ada3c
Jorge Moncayo Paz <84457471+jlmoncay@users.noreply.github.com>
2024-06-19 13:37:19 -0500

    Update hello_world_main.c

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -13,9 +13,28 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+/*#define CONFIG_EZM_SERVICE_COLECTOR_LOG
+[7A] [7A] [7A] [7A]
+*/
+#ifdef CONFIG_EZM_SERVICE_COLECTOR_LOG
+    static const char*          TAG = "colector.c";
+    #define print_serial_log(format, ... )              ESP_LOG_LEVEL_LOCAL(ESP_LOG_INFO, TAG, "[INFO] %s: Line = %d " format "\n", __func__, __LINE__, ##__VA_ARGS__)
+    #define print_serial_log_warning(format, ... )      ESP_LOG_LEVEL_LOCAL(ESP_LOG_WARN, TAG, "[WARN] %s:  Line = %d " format "\n", __func__, __LINE__, ##__VA_ARGS__)
+    #define print_serial_log_error(format, ... )        ESP_LOG_LEVEL_LOCAL(ESP_LOG_ERROR, TAG, "[ERROR] %s:  Line = %d " format "\n", __func__, __LINE__ , ##__VA_ARGS__)
+    #define print_serial_log_buffer(data,datalen)       ESP_LOG_BUFFER_HEXDUMP(TAG,data,datalen,ESP_LOG_ERROR);
+#else
+    #define print_serial_log(format, ... )              printf("[7A] [%d] [%s] [OK] [7A] " format "\n", __LINE__, __func__,  ##__VA_ARGS__)
+    #define print_serial_log_warning(format, ... )      printf("[7A] [%d] [%s] [W] [7A] " format "\n", __LINE__, __func__,  ##__VA_ARGS__)
+    #define print_serial_log_error(format, ... )        printf("[7A] [%d] [%s] [ERR] [7A] " format "\n", __LINE__, __func__,  ##__VA_ARGS__)
+    #define print_serial_log_buffer(data,datalen)       printf("[7A] [%d] [%s] [BUFF] [7A] " format "\n", __LINE__, __func__,  ##__VA_ARGS__)
+#endif
+/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
+   or you can edit the following line and set a number here.
+*/
+
 void app_main(void)
 {
-    printf("[ERR]\n");
+    print_serial_log_error("Esto es un error");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
